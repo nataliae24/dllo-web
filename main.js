a6124f385561cb3ee13f9623d0e5c06156b34b8a
Natalia Estrada <n.estrada@accenture.com>
2023-04-18 19:56:46 +0200

    add method load video.

---
+++ main.js
@@ -0,0 +1,37 @@
+if (window.File && window.FileReader && window.FileList) {
+
+  function loadVideo(inputFile) {
+    const video = document.getElementById("videoId");
+    validateFileType(inputFile);
+    video.onerror = function (e) {
+      alert("Error invalid format video. Accepted: MP4 | Webm | Ogg");
+    };
+
+    if (inputFile) {
+      inputFile.addEventListener("change", function () {
+        const file = inputFile.files[0];
+        const videourl = URL.createObjectURL(file);
+        video.setAttribute("src", videourl);
+        play(video);
+      });
+    }
+  }
+
+  function play(video) {
+    video.play();
+  }
+
+  function pause(video) {
+    video.pause();
+  }
+
+  const fileTypes = ["video/mp4", "video/webm", "video/ogg"];
+
+  function validateFileType(file) {
+    return fileTypes.includes(file.type);
+  }
+       
+} else {
+    alert("La Api File no es soportado en el navegador.");
+}
+
